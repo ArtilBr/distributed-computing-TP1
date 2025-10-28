a5cd0db2a914b9e83dc43cf5e76dc4dd6bb6ec70
Arthur Oliveira Braga <arthuroliveirabraga.12@gmail.com>
2025-10-27 21:09:15 -0300

    printer_server

---
+++ server/printer_server.py
@@ -0,0 +1,38 @@
+# printer_server.py
+import time
+import random
+from concurrent import futures
+import grpc
+import printing_pb2
+import printing_pb2_grpc
+
+class PrintingServiceServicer(printing_pb2_grpc.PrintingServiceServicer):
+    def SendToPrinter(self, request, context):
+        # A impressão usa diretamente os dados do request 
+        print(f"[TS: {request.lamport_timestamp}] CLIENTE {request.client_id}: {request.message_content}")
+        
+        # Simula tempo de impressão
+        time.sleep(random.uniform(2, 3))
+        
+        # Cria a resposta
+        resp = printing_pb2.PrintResponse(
+            success=True,
+            confirmation_message=f"Impressão concluída para cliente {request.client_id}",
+            # Apenas ecoa o timestamp do cliente
+            lamport_timestamp=request.lamport_timestamp 
+        )
+        return resp
+
+def serve(port=50051):
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    printing_pb2_grpc.add_PrintingServiceServicer_to_server(PrintingServiceServicer(), server)
+    server.add_insecure_port(f'[::]:{port}')
+    server.start()
+    print(f"Printer server rodando na porta :{port}")
+    try:
+        server.wait_for_termination()
+    except KeyboardInterrupt:
+        print("Printer server encerrado.")
+
+if __name__ == "__main__":
+    serve()
